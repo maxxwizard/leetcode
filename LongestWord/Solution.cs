a723e5ed27d09461847e7a6da3f6b3c775b516d0
Matthew Huynh <mahuynh@microsoft.com>
2019-06-29 11:07:11 -0700

    LongestWord reimplemented with brute force algorithm

---
+++ LongestWord/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LongestWord
+{
+    class Solution
+    {
+        public string LongestWord(string[] words)
+        {
+            var longestWord = string.Empty;
+
+            // sort the array
+            Array.Sort(words);
+
+            // walk the sorted array and add word only if its prefix already in set
+            var set = new HashSet<string>();
+            foreach (var word in words)
+            {
+                if (word.Length == 1 || set.Contains(word.Substring(0, word.Length - 1)))
+                {
+                    set.Add(word);
+                    if (word.Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+            }
+
+            return longestWord;
+        }
+    }
+}
--- LongestWord/UnitTests.cs
+++ LongestWord/UnitTests.cs
@@ -8,7 +8,7 @@ namespace LongestWord
         [TestMethod]
         public void Apple()
         {
-            var trie = new Trie(new string[]
+            Assert.AreEqual("apple", new Solution().LongestWord(new string[]
             {
                 "a",
                 "banana",
@@ -17,52 +17,42 @@ namespace LongestWord
                 "ap",
                 "apply",
                 "apple",
-            });
-
-            Assert.AreEqual("apple", trie.getLongestWord());
+            }));
         }
 
         [TestMethod]
         public void Latte()
         {
-            var trie = new Trie(new string[]
+            Assert.AreEqual("latte", new Solution().LongestWord(new string[]
             {
                 "m", "mo", "moc", "moch", "mocha", "l", "la", "lat", "latt", "latte", "c", "ca", "cat",
-            });
-
-            Assert.AreEqual("latte", trie.getLongestWord());
+            }));
         }
 
         [TestMethod]
         public void yodn()
         {
-            var trie = new Trie(new string[]
+            Assert.AreEqual("yodn", new Solution().LongestWord(new string[]
             {
                 "yo", "ew", "fc", "zrc", "yodn", "fcm", "qm", "qmo", "fcmz", "z", "ewq", "yod", "ewqz", "y"
-            });
-
-            Assert.AreEqual("yodn", trie.getLongestWord());
+            }));
         }
 
         [TestMethod]
         public void B()
         {
-            var trie = new Trie(new string[]
+            Assert.AreEqual("b", new Solution().LongestWord(new string[]
             {
                 "b",
-            });
-
-            Assert.AreEqual("b", trie.getLongestWord());
+            }));
         }
 
         [TestMethod]
         public void Empty()
         {
-            var trie = new Trie(new string[]
+            Assert.AreEqual("", new Solution().LongestWord(new string[]
             {
-            });
-
-            Assert.AreEqual("", trie.getLongestWord());
+            }));
         }
     }
 }
